cc8f3068fe9f888268031e05b3f79bb9b1b9315f
Byron Griffith <byrongriffith91@gmail.com>
2017-11-03 18:47:53 -0400

    finished

---
+++ keys.js
@@ -0,0 +1,10 @@
+// console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '4AqeoJkI2hbWEbWrq1Bp5w9DN',
+  consumer_secret: 'PJ4AMTlmR9etCmksqq2mchkSQ4oW8UzJf0geyneH0zxGxPRNz7',
+  access_token_key: '923309378828161024-OxGfehm4dAqKA1xukHBrcQwTAgA2CEX',
+  access_token_secret: 'enekcPFmpCnlPImjmVCHek3rkrH2CbSq3tYigHDiGXj5K',
+}
+
+module.exports = twitterKeys;
